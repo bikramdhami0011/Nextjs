b904e5798cd987603df9d31525e1ff6abeee6a49
bikram dhami <bikramdhami296@gmail.com>
2023-06-15 21:57:43 +0545

    not-found page

---
+++ src/app/about/[...not-found]/page.js
@@ -0,0 +1,6 @@
+const NotFound=()=>{
+    return (
+        <h1> this page is not found in about pages</h1>
+    )
+}
+export default NotFound;
---
+++ src/app/not-found.js
@@ -0,0 +1,6 @@
+const NotFound=()=>{
+    return (
+        <h1> this page is not found in this click</h1>
+    )
+}
+export default NotFound;
